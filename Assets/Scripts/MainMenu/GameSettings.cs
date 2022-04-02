2aa88c4397c66be88dc506287cd7415bbcff0e13
Alex.Ag <alexppcastro22@gmail.com>
2022-04-02 19:16:22 -0300

    Added resolution selector

--- Assets/Scripts/MainMenu/GameSettings.cs
+++ Assets/Scripts/MainMenu/GameSettings.cs
@@ -3,31 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
+using System.Linq;
 
 namespace AgTech
 {
     public class GameSettings : MonoBehaviour
     {
-        public AudioMixer sfxMixer;
-        public AudioMixer musicMixer;
+        public AudioMixer audioMixer;
+        public TMP_Dropdown resolutionDropdown;
+        
+        private Resolution[] resolutions;
+        private List<string> resolutionOptions = new List<string>();
+        private void Start() 
+        {      
+            SetUpResolutionDropdown();
+        }
+
         public void ToggleFullscreen(bool isFullscreen)
         {
             Screen.fullScreen = isFullscreen;
         }
 
-        public void SetResolution()
+        private void SetUpResolutionDropdown()
         {
+            resolutions = Screen.resolutions.Where(resolution => resolution.refreshRate == 60).ToArray();
+            resolutionDropdown.ClearOptions();
+            
+            int currentIndex = 0;
+            for ( int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                resolutionOptions.Add(option);
 
+                if(resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentIndex = i;
+                }
+            }
+
+            resolutionDropdown.AddOptions(resolutionOptions);
+            resolutionDropdown.value = currentIndex;
+            resolutionDropdown.RefreshShownValue();
+        }
+
+        public void SetResolution(int index)
+        {
+            Resolution resolution = resolutions[index];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
         }
 
         public void SetSFXVolume(float volume)
         {
-            sfxMixer.SetFloat("SFXVolume", volume);
+            audioMixer.SetFloat("SFXVolume", volume);
         }
 
         public void SetMusicVolume(float volume)
         {
-            sfxMixer.SetFloat("MusicVolume", volume);
+            audioMixer.SetFloat("MusicVolume", volume);
         }
 
         public void ToggleCredits()
